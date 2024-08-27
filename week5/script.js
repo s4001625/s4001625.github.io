6a39643e24f30a6bbcc4b15233888bd2d08c075f
s4001625 <s4001625@student.rmit.edu.au>
2024-08-27 11:00:50 +1000

    week5 update

---
+++ week5/script.js
@@ -0,0 +1,28 @@
+console.log("welcome to the class");
+// this is a comment
+let myName = "alice";
+const myCity = "Melbourne";
+console.log(myName, "lives in", myCity);
+console.log(`${myName} lives in ${myCity}`);
+
+let count = 10;
+console.log(count);
+
+let Isitmorningclass = true;
+let IsItAfternoonClass = false;
+
+let myStudentRecord = {
+  name: "Michelle",
+  id: 1234,
+  class: "OART2301",
+  isItscience: false,
+  isitdesign: true,
+};
+
+console.log("Hello everyone, my name is", myStudentRecord.name);
+console.log("my id is", myStudentRecord.id);
+console.log("my class is", myStudentRecord.class);
+console.log("are you science student?", myStudentRecord.isItscience);
+
+const numberArray = [2, 4, 6, 8];
+console.log(numberArray[2]);
