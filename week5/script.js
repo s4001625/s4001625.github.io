ab509fca52d5a6210ae0b5f8ee66f58010b22f15
s4001625 <s4001625@student.rmit.edu.au>
2024-08-27 12:29:49 +1000

    updates

--- week5/script.js
+++ week5/script.js
@@ -1,48 +1,48 @@
-console.log("welcome to the class");
-// this is a comment
-let myName = "alice";
-const myCity = "Melbourne";
-console.log(myName, "lives in", myCity);
-console.log(`${myName} lives in ${myCity}`);
+// console.log("welcome to the class");
+// // this is a comment
+// let myName = "alice";
+// const myCity = "Melbourne";
+// console.log(myName, "lives in", myCity);
+// console.log(`${myName} lives in ${myCity}`);
 
-let count = 10;
-console.log(count);
+// let count = 10;
+// console.log(count);
 
-let Isitmorningclass = true;
-let IsItAfternoonClass = false;
+// let Isitmorningclass = true;
+// let IsItAfternoonClass = false;
 
-let myStudentRecord = {
-  name: "Michelle",
-  id: 1234,
-  class: "OART2301",
-  isItscience: false,
-  isitdesign: true,
-};
+// let myStudentRecord = {
+//   name: "Michelle",
+//   id: 1234,
+//   class: "OART2301",
+//   isItscience: false,
+//   isitdesign: true,
+// };
 
-console.log("Hello everyone, my name is", myStudentRecord.name);
-console.log("my id is", myStudentRecord.id);
-console.log("my class is", myStudentRecord.class);
-console.log("are you science student?", myStudentRecord.isItscience);
+// console.log("Hello everyone, my name is", myStudentRecord.name);
+// console.log("my id is", myStudentRecord.id);
+// console.log("my class is", myStudentRecord.class);
+// console.log("are you science student?", myStudentRecord.isItscience);
 
-const numberArray = [2, 4, 6, 8];
-console.log(numberArray[2]);
-myStudentRecord.isItscience = true;
-if (myStudentRecord.isItscience) {
-  console.log("sorry you're in the wrong class");
-} else {
-  console.log("Welcome to OART1013");
-}
+// const numberArray = [2, 4, 6, 8];
+// console.log(numberArray[2]);
+// myStudentRecord.isItscience = true;
+// if (myStudentRecord.isItscience) {
+//   console.log("sorry you're in the wrong class");
+// } else {
+//   console.log("Welcome to OART1013");
+// }
 
-let myScore = 60;
-if (myScore >= 90) {
-  console.log("You scored a HD");
-} else if (myScore < 90 && myScore >= 70) {
-  console.log("You missed HD");
-} else if (myScore < 70 && myScore >= 50) {
-  console.log("You score a C");
-} else {
-  console.log("You score a P");
-}
+// let myScore = 60;
+// if (myScore >= 90) {
+//   console.log("You scored a HD");
+// } else if (myScore < 90 && myScore >= 70) {
+//   console.log("You missed HD");
+// } else if (myScore < 70 && myScore >= 50) {
+//   console.log("You score a C");
+// } else {
+//   console.log("You score a P");
+// }
 
 // == equal to
 // === equal value and equal type
@@ -57,3 +57,15 @@ if (myScore >= 90) {
 //&& and
 // || or
 // ! not
+
+// console.log("Hello Martha");
+// console.log("Hello Maria");
+// console.log("Hello Mary");
+// console.log("Hello Bathsheba");
+// console.log("Hello Thomas");
+
+const names = ["Martha", "Maria", "Mary", "Bathsheba", "Thomas"];
+console.log(names.length);
+for (let i = 0; i < names.length; i++) {
+  console.log("Hello", names[i]);
+}
--- week5/weather/script.js
+++ week5/weather/script.js
@@ -8,9 +8,13 @@ function checkWeather() {
     outer.style.backgroundColor = "blue";
     console.log("it is burning hot");
   } else if (temp.value < 40 && temp.value >= 30) {
-    body.style.backgroundColor = "aqua";
+    body.style.backgroundColor = "limegreen";
     outer.style.backgroundColor = "peach";
-    console.log("it is a pleasant weather");
+    console.log("it is sunny and warm");
+  } else if (temp.value < 30 && temp.value >= 15) {
+    body.style.backgroundColor = "aqua";
+    outer.style.backgroundColor = "purple";
+    console.log("it is pleasant weather");
   } else {
     body.style.backgroundColor = "grey";
     outer.style.backgroundColor = "purple";
