984ccec4bbfe41273e68755ef5a98cd0995c0a6b
s4001625 <s4001625@student.rmit.edu.au>
2024-08-27 12:11:00 +1000

    updet

--- week5/weather/script.js
+++ week5/weather/script.js
@@ -1,15 +1,19 @@
 function checkWeather() {
   let temp = document.querySelector("#temp");
   let body = document.querySelector("body");
+  let outer = document.querySelector(".outer");
   console.log(temp.value);
   if (temp.value >= 40) {
     body.style.backgroundColor = "red";
+    outer.style.backgroundColor = "blue";
     console.log("it is burning hot");
   } else if (temp.value < 40 && temp.value >= 30) {
     body.style.backgroundColor = "aqua";
+    outer.style.backgroundColor = "peach";
     console.log("it is a pleasant weather");
   } else {
     body.style.backgroundColor = "grey";
+    outer.style.backgroundColor = "purple";
     console.log("it is freezing");
   }
 }
