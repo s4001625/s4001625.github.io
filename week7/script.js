c18adf49a723816eb33a2558879b4584b543e3f8
s4001625 <s4001625@student.rmit.edu.au>
2024-09-17 11:58:43 +1000

    week7 update

--- week7/script.js
+++ week7/script.js
@@ -17,12 +17,17 @@ console.log(pauseButton);
 
 const notify = document.querySelector("#notify");
 console.log(notify);
-playButton.addEventListener("click", playNotify);
-function playNotify() {
-  notify.play();
+
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+playButton.addEventListener("click", playVideo);
+function playVideo() {
+  //   notify.play();
+  myVideo.play();
 }
 
-pauseButton.addEventListener("click", pauseNotify);
-function pauseNotify() {
-  notify.pause();
+pauseButton.addEventListener("click", pauseVideo);
+function pauseVideo() {
+  //   notify.pause();
+  myVideo.pause();
 }
