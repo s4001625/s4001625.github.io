6f50c2e48954c914ebdad5200afe99d0f6695221
s4001625 <s4001625@student.rmit.edu.au>
2024-09-17 12:27:24 +1000

    update

---
+++ week7/mediaplayer/script.js
@@ -0,0 +1,20 @@
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseBotton = document.querySelector("#play-pause-button");
+console.log(playPauseBotton);
+
+playPauseBotton.addEventListener("click", playPauseMe);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+
+function playPauseMe() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  }
+}
