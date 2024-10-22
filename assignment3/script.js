da8bc044906a446788bf6f0b53fb4be1e8ae88b2
s4001625 <s4001625@student.rmit.edu.au>
2024-10-22 15:14:46 +1100

    updates

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,18 +1,68 @@
-const paperClose = document.querySelector("#paper-close");
-console.log(paperClose);
+// const paperClose = document.querySelector("#paper-close");
+// console.log(paperClose);
 const paperOpen = document.querySelector("#paper-open");
 console.log(paperOpen);
-const plastiClose = document.querySelector("#plastic-close");
-console.log(plastiClose);
+// const plastiClose = document.querySelector("#plastic-close");
+// console.log(plastiClose);
 const plasticOpen = document.querySelector("#plastic-open");
 console.log(plasticOpen);
-const paperBag = document.querySelector("#paper-bag");
-console.log(paperBag);
-const crumPaper = document.querySelector("#crump-paper");
-console.log(crumPaper);
-const plastiCup = document.querySelector("#plastic-cup");
-console.log(plastiCup);
-const plasticBag = document.querySelector("#plastic-bag");
-console.log(plasticBag);
-const plasticBottle = document.querySelector("#plastic-bottle");
-console.log(plasticBottle);
+// const paperBag = document.querySelector("#paper-bag");
+// console.log(paperBag);
+// const crumPaper1 = document.querySelector("#crump-paper1");
+// console.log(crumPaper1);
+// const crumPaper2 = document.querySelector("#crump-paper2");
+// console.log(crumPaper2);
+// const plastiCup = document.querySelector("#plastic-cup");
+// console.log(plastiCup);
+// const plasticBag = document.querySelector("#plastic-bag");
+// console.log(plasticBag);
+// const plasticBottle = document.querySelector("#plastic-bottle");
+// console.log(plasticBottle);
+
+let draggedElement = null;
+const dragItems = document.querySelectorAll(".draggable");
+
+for (let i = 0; i < dragItems.length; i++) {
+  dragItems[i].addEventListener("dragstart", function () {
+    draggedElement = dragItems[i];
+    // dragItems[i].style.display = "none";
+  });
+  dragItems[i].addEventListener("dragend", function () {
+    dragItems[i].style.display = "none";
+  });
+}
+
+paperOpen.addEventListener("dragover", endDrag);
+function endDrag(event) {
+  const category = event.getAttribute("data-id");
+  console.log(category);
+  event.preventDefault();
+  //   event.style.display = "none";
+}
+
+paperOpen.addEventListener("drop", handlePaperDrop);
+
+plasticOpen.addEventListener("dragover", endDrag);
+function endDrag() {
+  event.preventDefault();
+}
+
+plasticOpen.addEventListener("drop", handlePlasticDrop);
+
+function handlePaperDrop() {
+  const category = draggedElement.getAttribute("data-id");
+  console.log(category);
+  if (draggedElement && category === "paper") {
+    draggedElement.style.display = "none";
+    draggedElement = null;
+  } else {
+    alert("wrong bin");
+  }
+}
+
+function handlePlasticDrop() {
+  if (draggedElement) {
+    draggedElement.style.display = "none";
+    draggedElement = null;
+  }
+}
